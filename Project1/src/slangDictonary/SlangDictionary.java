520ec28de7b19292a70509f1a63bcc850c204c6e
dangvuminh <dangvuminh98@gmail.com>
2020-11-23 20:05:42 +0700

    screate classes

---
+++ Project1/src/slangDictonary/SlangDictionary.java
@@ -0,0 +1,7 @@
+package slangDictonary;
+
+public class SlangDictionary {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Project1/src/slangDictonary/SlangWord.java
@@ -0,0 +1,6 @@
+package slangDictonary;
+
+public class SlangWord {
+	String slangWord;
+	String definition;
+}
